c6cc057c2c71034fba245bd1e142dfb17cb71057
hinoob <redisolenmina1@gmail.com>
2025-09-10 08:57:05 +0300

    Fixed python code & added tests

--- python/kood.py
+++ python/kood.py
@@ -1,10 +1,21 @@
 def matemaatilised_arvutused(a, b):
     summa = a + b
-    vahe = a - a
+    vahe = a - b
     jagatis = a // b
-    korrutis = a * a
+    korrutis = a * b
 
     return summa, vahe, jagatis, korrutis
 
 print(matemaatilised_arvutused(10, 5))
 # oodatav vastus: (15, 5, 2, 50)
+
+summa, vahe, jagatis, korrutis = matemaatilised_arvutused(10, 5)
+# test
+assert summa == 15
+print("Summa on õige")
+assert vahe == 5
+print("Vahe on õige")
+assert jagatis == 2
+print("Jagatis on õige")
+assert korrutis == 50
+print("Korrutis on õige")
